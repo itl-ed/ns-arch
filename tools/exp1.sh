e24245ec9469717e5cb064b3d2d644fc70f1c946
jpstyle <pjh4g92@gmail.com>
2022-07-02 02:24:30 +0100

    Misc.

--- tools/exp1.sh
+++ tools/exp1.sh
@@ -9,7 +9,7 @@ do
     do
         for ((i=$1;i<=$2;i++))
         do
-            COMM="python tools/exp1.py -ic -lp wandb://jpstyle/vision_vg_scenegraph/1eg0ctph/checkpoints/epoch\=9-distro.ckpt -x1df $diff -x1tf $tstrat -x1rs $i -op '$3/ns-arch/output' > $3/ns-arch/log_${diff}_${tstrat}_zeroInit_${i}.txt"
+            COMM="python tools/exp1.py -ic -lp wandb://jpstyle/vision_vg_scenegraph/1eg0ctph/checkpoints/epoch\=9-distro.ckpt -x1df $diff -x1tf $tstrat -x1rs $i -op '$3/ns-arch/output' > $3/ns-arch/output/log_${diff}_${tstrat}_zeroInit_${i}.txt"
             eval $COMM
         done
     done
